21119978f9ad5f790234c4f9a44408143559a5b3
menintel <menintel@tuta.io>
2025-05-05 10:15:46 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Navbar from './sections/navbar'
+import Hero from './sections/Hero'
+
+const App = () => {
+  return (
+    <div className="container mx-auto max-w-7xl">
+      <Navbar/>
+      <Hero/>
+      <section className='min-h-screen' />
+      <section className='min-h-screen' />
+      <section className='min-h-screen' />
+      <section className='min-h-screen' />
+      <section className='min-h-screen' />
+      <section className='min-h-screen' />
+      <section className='min-h-screen' />
+      {/* about */}
+      {/* projects */}
+      {/* testimonial */}
+      {/* contact */}
+      {/* footer */}
+    </div>
+  )
+}
+
+export default App
